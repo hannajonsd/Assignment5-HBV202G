1693672546ce9e32d75ba9098a35b37f1230fbd0
vikoand <voa6@hi.is>
2023-02-20 12:58:54 +0000

    JavaDoc bætti við

--- src/test/java/is/hi/hbv202g/ass5/IntStackTest.java
+++ src/test/java/is/hi/hbv202g/ass5/IntStackTest.java
@@ -8,18 +8,23 @@ import org.junit.Test;
 public class IntStackTest {
 
     IntStack stack;
+
     @Before
     public void createEmptyStack(){
         stack = new IntStack();
     }
 
-
+    /**
+     * Test that checks if the new stack is not full.
+     */
     @Test
     public void testNewStackIsNotFull() {
         assertFalse(stack.isFull());
     }
 
-
+    /**
+     * Test that checks if a full stack is indeed full.
+     */
     @Test
     public void testFullStackIsFull() {
         int c = stack.getCapacity();
@@ -30,7 +35,9 @@ public class IntStackTest {
         assertTrue(stack.isFull());
     }
 
-
+    /**
+     * Test that checks if an almost full stack is not full.
+     */
     @Test
     public void testAlmostFullStack() {
         int c = stack.getCapacity();
@@ -41,6 +48,9 @@ public class IntStackTest {
         assertFalse(stack.isFull());
     }
 
+    /**
+     * Test that checks that the value that has been pushed is later popped again, checks if it is the same value.
+     */
     @Test
     public void testPopReturnsPushedValue() {
         int n = 2;
@@ -49,12 +59,17 @@ public class IntStackTest {
         assertEquals(n,m);
     }
 
+    /**
+     * Test that checks if pop on an empty stack returns an exeption ArrayIndexOutOfBoundsException.
+     */
     @Test(expected=ArrayIndexOutOfBoundsException.class)
     public void testExceptionPoppingEmptyStack() {
         stack.pop();
     }
 
-
+    /**
+     * Test that checks if push on full stack returns an exception ArrayIndexOutOfBoundsExeption.
+     */
     @Test(expected=ArrayIndexOutOfBoundsException.class)
     public void testExceptionPushingFullStack() {
         int c = stack.getCapacity();
@@ -65,12 +80,17 @@ public class IntStackTest {
         stack.push(c);
     }
 
+    /**
+     * Test that checks if an empty stack is empty.
+     */
     @Test
     public void testEmptyStackIsEmpty() {
         assertTrue(stack.isEmpty());
     }
 
-
+    /**
+     * Test that checks if an almost empty stack says it is empty.
+     */
     @Test
     public void testAlmostEmptyStack() {
         for (int i = 0; i < 1; i++) {
